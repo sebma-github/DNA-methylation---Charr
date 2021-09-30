3bfecb374a0a62d6919bd0b5738a52fe3dca7811
sebma-github <47628431+sebma-github@users.noreply.github.com>
2021-09-30 13:27:55 +0000

    Update getmethylcoverage.sh

--- getmethylcoverage.sh
+++ getmethylcoverage.sh
@@ -5,6 +5,9 @@
 /programs/bbmap/bbduk.sh in=$read1 in2=$read2 out=R1clean.fq out2=R2clean.fq qtrim=r trimq=10 &&
 
 #Trim for adapter and PhiX sequences
+adapters = ~/adapters.fasta
+PhiX = ~/PhiX.fasta
+
 /programs/bbmap/bbduk.sh in=R1clean.fq in2=R2clean.fq out=R1_1clean.fq out2=R2_1clean.fq ref=$adapters,$PhiX ktrim=r
 k=15 tbo=f tpe=f  &&
 
