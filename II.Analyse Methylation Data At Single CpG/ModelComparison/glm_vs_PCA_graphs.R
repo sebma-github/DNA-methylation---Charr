41d7142c56a40e4699a5be0c2a4706f974ec9c0c
sebma-github <47628431+sebma-github@users.noreply.github.com>
2022-07-07 20:17:58 +0200

    Update glm_vs_PCA_graphs.R

--- II.Analyse Methylation Data At Single CpG/ModelComparison/glm_vs_PCA_graphs.R
+++ II.Analyse Methylation Data At Single CpG/ModelComparison/glm_vs_PCA_graphs.R
@@ -1,43 +1,37 @@
 # Make graphs of pvalues by glm ~ weight on PC
 library(ggplot2)
 library(ggpubr)
-#Load glm data
-glmfinal <- read.csv("/Users/sebmatlosz/Desktop/methylation/glm_10340res_pvalues_corrected.csv")
-
-# glmfinal <- read.csv("/Users/sebmatlosz/Desktop/methylation/glm_10340res_Niche_pvalues_corrected.csv")
-#         glmfinal$Morph_pvalue <- glmfinal$Niche_pvalue
-#         glmfinal$Morph_pvalue_corrected <- glmfinal$Niche_pvalue_corrected
-
-#Load PCA data
-PCAfinal <- read.csv("/Users/sebmatlosz/Desktop/methylation/weightsonPC_48samplesPCA.csv")
-  
-#Merge data frames based on residues with sqldf?
 library(sqldf)
 
-#Only keep the few PCs of interest / ATH Change Niche into Morphs or vice versa
-df <- sqldf('SELECT Residue, Morph_pvalue, Morph_pvalue_corrected, Timepoint_pvalue, Timepoint_pvalue_corrected, Sex_pvalue, 
-              Sex_pvalue_corrected, PC1, PC4, PC23,ID
-             FROM glmfinal, PCAfinal
-             WHERE Residue=ID')
+#Load glm data
+    glmfinal <- read.csv("~/GLM/results/glm_10340res_pvalues_withandwithout_Bonfcorrection.csv")
+
+#Load PCA data
+    PCAfinal <- read.csv("~/PCA/results/weightsonPC_48samplesPCA.csv")
+  
+#Merge data frames based on residues
+#Only keep the few PCs of interest
+    df <- sqldf('SELECT Residue, Morph_pvalue, Morph_pvalue_corrected, Timepoint_pvalue, Timepoint_pvalue_corrected, Sex_pvalue, 
+                  Sex_pvalue_corrected, PC1, PC4, PC23,ID
+                 FROM glmfinal, PCAfinal
+                 WHERE Residue=ID')
 
 #Change the PC weights into absolute values
-df$PC1 <- abs(df$PC1)
-df$PC4 <- abs(df$PC4)
-df$PC23 <- abs(df$PC23)
+    df$PC1 <- abs(df$PC1)
+    df$PC4 <- abs(df$PC4)
+    df$PC23 <- abs(df$PC23)
 
 #Change the pvalues into -log
-df$Morph_pvalue_corrected_log <- -log(df$Morph_pvalue_corrected)
-df$Timepoint_pvalue_corrected_log <- -log(df$Timepoint_pvalue_corrected)
-df$Sex_pvalue_corrected_log <- -log(df$Sex_pvalue_corrected)
+    df$Morph_pvalue_corrected_log <- -log(df$Morph_pvalue_corrected)
+    df$Timepoint_pvalue_corrected_log <- -log(df$Timepoint_pvalue_corrected)
+    df$Sex_pvalue_corrected_log <- -log(df$Sex_pvalue_corrected)
 
-df$Morph_pvalue_log <- -log(df$Morph_pvalue)
-df$Timepoint_pvalue_log <- -log(df$Timepoint_pvalue)
-df$Sex_pvalue_log <- -log(df$Sex_pvalue)
+    df$Morph_pvalue_log <- -log(df$Morph_pvalue)
+    df$Timepoint_pvalue_log <- -log(df$Timepoint_pvalue)
+    df$Sex_pvalue_log <- -log(df$Sex_pvalue)
 
 
 ###Check the correlations
-#I need to start by checking if the data is parametric or not
-qqnorm(df$Morph_pvalue_corrected_log) #Clearly not, because it is in log and stuff.
 
 correlation <- function(x,y,t) {
   title=paste0(t)
@@ -48,43 +42,35 @@ correlation <- function(x,y,t) {
   return(z)
 }
 
-Morphs <- correlation(x = 'PC4',y = 'Morph_pvalue_corrected_log', "Correlation PCA vs glm / Morphs")
-Morphs
+    Morphs <- correlation(x = 'PC4',y = 'Morph_pvalue_corrected_log', "Correlation PCA vs glm / Morphs")
+    Morphs
+    Morphcorrnumber <- cor.test(df$PC4, df$Morph_pvalue_corrected_log, method = "kendall")
+    Morphcorrnumber$p.value
+    Morphcorrnumber$estimate
 
-#Morphcorrnumber <- cor.test(df$PC4, df$Morph_pvalue_corrected_log, method = "pearson")
-Morphcorrnumber <- cor.test(df$PC4, df$Morph_pvalue_corrected_log, method = "kendall")
-Morphcorrnumber$p.value
-Morphcorrnumber$estimate
-
-Times <- correlation(x = 'PC1',y = 'Timepoint_pvalue_corrected_log', "Correlation PCA vs glm / Timepoints")
-Times
-
-#Timecorrnumber <- cor.test(df$PC1, df$Timepoint_pvalue_corrected_log, method = "pearson")
-Timecorrnumber <- cor.test(df$PC1, df$Timepoint_pvalue_corrected_log, method = "kendall")
-Timecorrnumber$p.value
-Timecorrnumber$estimate
-
+    Times <- correlation(x = 'PC1',y = 'Timepoint_pvalue_corrected_log', "Correlation PCA vs glm / Timepoints")
+    Times
+    Timecorrnumber <- cor.test(df$PC1, df$Timepoint_pvalue_corrected_log, method = "kendall")
+    Timecorrnumber$p.value
+    Timecorrnumber$estimate
 
 
 #Plot things
-library(ggplot2)
-library(ggpubr)
+    timepointgraph <- ggplot(df, aes(x=PC1, y=Timepoint_pvalue_corrected_log)) + geom_point() + theme_bw() + 
+      ggtitle("Residues affecting timepoints - Model comparison") + labs(x="Weight on PC1",y="Timepoint -log(pvalue) in glm")
+    timepointgraph
 
-timepointgraph <- ggplot(df, aes(x=PC1, y=Timepoint_pvalue_corrected_log)) + geom_point() + theme_bw() + 
-  ggtitle("Residues affecting timepoints - Model comparison") + labs(x="Weight on PC1",y="Timepoint -log(pvalue) in glm")
-timepointgraph
+    morphgraph <- ggplot(df, aes(x=PC4, y=Morph_pvalue_corrected_log)) + geom_point() + theme_bw() + 
+      ggtitle("Residues affecting Niches - Model comparison") + labs(x="Weight on PC4",y="Niche -log(pvalue) in glm")
+    morphgraph
 
-morphgraph <- ggplot(df, aes(x=PC4, y=Morph_pvalue_corrected_log)) + geom_point() + theme_bw() + 
-  ggtitle("Residues affecting Niches - Model comparison") + labs(x="Weight on PC4",y="Niche -log(pvalue) in glm")
-morphgraph
+    sexgraph <- ggplot(df, aes(x=PC23, y=Sex_pvalue_corrected_log)) + geom_point() + theme_bw() + 
+      ggtitle("Residues affecting sex - Model comparison") + labs(x="Weight on PC23",y="Sex -log(pvalue) in glm")
+    sexgraph
 
-sexgraph <- ggplot(df, aes(x=PC23, y=Sex_pvalue_corrected_log)) + geom_point() + theme_bw() + 
-  ggtitle("Residues affecting sex - Model comparison") + labs(x="Weight on PC23",y="Sex -log(pvalue) in glm")
-sexgraph
+    figure <- ggarrange(timepointgraph,morphgraph,sexgraph, ncol=2, nrow=2)
+    figure
 
-figure <- ggarrange(timepointgraph,morphgraph,sexgraph, ncol=2, nrow=2)
-figure
-
-pdf("/Users/sebmatlosz/Desktop/Figuresforpapers/ModelComparisons_Niche_minuslog_corrected_glmPCA_090222.pdf")
+pdf("~/results/ModelComparisons_minuslog_corrected_glmPCA_090222.pdf")
 figure
 dev.off()
