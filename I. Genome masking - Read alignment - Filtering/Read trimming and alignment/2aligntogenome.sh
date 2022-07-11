94e1daf71ac81b7c3f2c90bb21e1cd4012fca1a1
sebma-github <47628431+sebma-github@users.noreply.github.com>
2022-07-11 19:26:22 +0200

    Update and rename 2aligntogenome.txt to 2aligntogenome.sh

--- I. Genome masking - Read alignment - Filtering/Read trimming and alignment/2aligntogenome.sh
+++ I. Genome masking - Read alignment - Filtering/Read trimming and alignment/2aligntogenome.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #find in the folder all files that end in *_merged.fq, then use the function bismark on all of them one after the other
 #align them to the masked genome
 find ~/Trimmed_reads/ -type f -name \*_merged.fq -exec /programs/Bismark_v0.19.0/bismark --genome ~/genomemaskedwithCT {} \; &&
