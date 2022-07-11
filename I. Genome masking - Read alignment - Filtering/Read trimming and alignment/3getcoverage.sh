6f65e5691a64e016147272354bc6108f91fcaae1
sebma-github <47628431+sebma-github@users.noreply.github.com>
2022-07-11 19:06:16 +0200

    Update 3getcoverage.sh

--- I. Genome masking - Read alignment - Filtering/Read trimming and alignment/3getcoverage.sh
+++ I. Genome masking - Read alignment - Filtering/Read trimming and alignment/3getcoverage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-##the script must be run in the directory: "/data1/RRBS_results/Alignements/withallCT"
+##the script must be run in the directory where the alignments in .bam are (created from the 2aligntogenome.sh script).
 
 ##the files need to be processed in sam format and not bam
-##looks in a directory and transforms all .bam into .sam
+##Transform all .bam into .sam
 ls *bam | xargs -I {} -n 1 samtools view -h {} -o {}.sam
 
 wait
@@ -11,19 +11,16 @@ wait
 ##run bismark methylation_extractor on all files ending in .sam in the directory. Produces a lot of things, including the coverage reports.
 
 for f in ./*.sam; do
-	/programs/Bismark_v0.19.0/bismark_methylation_extractor --comprehensive --merge_non_CpG --samtools_path /programs/samtools-1.7/ --bedgraph --scaffolds --cytosine_report --genome_folder /data1/RRBS_results/SNP_stuff/genomemaskedwithCT "$f"
+	/programs/Bismark_v0.19.0/bismark_methylation_extractor --comprehensive --merge_non_CpG --samtools_path /programs/samtools-1.7/ --bedgraph --scaffolds --cytosine_report --genome_folder ~/genomemaskedwithCTSNPs "$f"
 done
 
 wait
 
 ## order all the "useless" output files so it is easier to see where we're at.
-
 mv *M-bias* -t ./Mbiastrash 
 wait
-
-mv *.txt -t ./getcoveragereportfiles/50ts
+mv *.txt -t ./getcoveragereportfiles/100ts
 wait
-
 mv *bedGraph* -t ./bedgraphfiles/
 wait
 
@@ -32,13 +29,14 @@ gzip -k -d *cov.gz
 wait 
 
 ##finish ordering everything
-mkdir ./coveragefiles/50ts       
+mkdir ./coveragefiles/100ts       
 wait
 
-mv *.cov* -t ./coveragefiles/50ts
+mv *.cov* -t ./coveragefiles/100ts
 wait
-
 
 mv *.bam* -t ./bamsamfiles
+
+## The .cov files are the one we will use for further analysis.
 
 
