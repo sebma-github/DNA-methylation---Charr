03b1930d06c91facbcb1ad654f88b7a2aabd1f56
sebma-github <47628431+sebma-github@users.noreply.github.com>
2022-07-11 21:50:37 +0200

    Update and rename extractgeneinfobetter.sh to extractmethinfo.sh

--- extractmethinfo.sh
+++ extractmethinfo.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-## This script allows me to extract the methylation information from all 48 sample files, at a specific 1000bp DMR in a specific chromosome
-## It was written to test my theory that global methylation over a 1000bp region could still be interpreted as 
-## semi-valid proof of the differences between morphs at different times. Or at least construct graphs.
-##Needs to be run in the directory where the .cov files are
+## This script allows to extract methylation information from all 48 sample files, at a specific 1000bp DMR in a specific scaffold.
+##Needs to be run in the directory where the .cov files are.
 
 #for i in {2..389}; #For DMRs between stages 
 for i in {2..141}; #For DMRs between morphs
@@ -11,25 +9,16 @@ for i in {2..141}; #For DMRs between morphs
 
 do
 	#gene=((i-1)) doesn't matter
-        name=$(awk '{ if(NR=='$i') print $1 }' /home/sebastien/tablesforextraction/DMRsbetweenmorphsforextraction.tsv)
-        scaff=$(awk '{ if(NR=='$i') print $2 }' /home/sebastien/tablesforextraction/DMRsbetweenmorphsforextraction.tsv)
-        postemp=$(awk '{ if(NR=='$i') print $3 }' /home/sebastien/tablesforextraction/DMRsbetweenmorphsforextraction.tsv)
-	pos=$(awk '{ if(NR=='$i') print $4 }' /home/sebastien/tablesforextraction/DMRsbetweenmorphsforextraction.tsv)
+        name=$(awk '{ if(NR=='$i') print $1 }' ~/DMRsbetweenmorphsforextraction.tsv)
+        scaff=$(awk '{ if(NR=='$i') print $2 }' ~/DMRsbetweenmorphsforextraction.tsv)
+        postemp=$(awk '{ if(NR=='$i') print $3 }' ~/DMRsbetweenmorphsforextraction.tsv)
+	pos=$(awk '{ if(NR=='$i') print $4 }' ~/DMRsbetweenmorphsforextraction.tsv)
 		
 		#Enter the scaffold number related to the DMR of interest
 		for f in ./*.cov; 
 		do
 			grep -n "$scaff" "$f" > ""$f"temp" 
 		done
-
-		#Enter the lines which match the coordinates indicated by the DMR of interest       
-		#for f in ./*.covtemp;     							#OLD VERSION, DO NOT USE
-		#do
-		#	awk '{if($2~/^'$pos'.../){print $0} else {}}' "$f" > ""$f"final"
-		#wait
-		#	mv ""$f"final" "$f"
-		#done
-		#wait
 
 		#Keep all lines that are above the start of the windowtile
                 for f in ./*.covtemp;
@@ -81,10 +70,7 @@ do
 		done
 	
 
-		#Merge all the files together based on their first column. This is a bad way of doing things because I should't have one file as a base, as I will only join positions that
-		#are only present in the first file.
-		#UNLESS I create a dummy file with a vector of every one of the thousand numbers. Then I am not losing any information.
-		
+	
 		#Put all files one after the other
 		cat *.covtemp > temp.covmodel
 		#Keep only the position column
@@ -97,16 +83,6 @@ do
 
 		#BEfore joining all the files together, I meed to replace the empty values with some string eg "NA"
 		#Otherwise if I join all the files, some of the numbers might get shifted to the left.
-		#To do so, need to join temp3.covmodel to each file individually.
-
-		for f in ./*.covtemp;
-		do
-			join <(sort temp3.covmodel) <(sort "$f") -a 1 | column -t | tac > individual.cov
-		wait
-			mv individual.cov "$f"
-		done
-
-		#Still that doesn't solve the problem because there ISNT any blank space in the first place !
 		#So  first let's retransform the data with 3 columns. 
 
 		for f in ./*.covtemp;
@@ -117,7 +93,6 @@ do
 		done			
 
 		#Now that there are 3 columns I can replace the blank space with missing values.
-		#The followimg line replaces the blank with 0 but I would like to replace it by a string.
 
 		for f in ./*.covtemp;
 		do
@@ -138,11 +113,8 @@ do
 		#Now in order for me to work on the data in R, I need to be sure that everything is tab sep.
 		#So I recreate all the columns manually
 
-		awk 'BEGIN{ OFS="\t"}{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60, $61, $62, $63, $64, $65, $66, $67, $68, $69, $70, $71, $72, $73, $74, $75, $76, $77, $78, $79, $80, $81, $82, $83, $84, $85, $86, $87, $88, $89, $90, $91, $92, $93, $94, $95, $96, $97}' temp3.covmodel > ./DMRtest/"$name".covdf
+		awk 'BEGIN{ OFS="\t"}{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60, $61, $62, $63, $64, $65, $66, $67, $68, $69, $70, $71, $72, $73, $74, $75, $76, $77, $78, $79, $80, $81, $82, $83, $84, $85, $86, $87, $88, $89, $90, $91, $92, $93, $94, $95, $96, $97}' temp3.covmodel > ~/"$name".covdf
 
-
-		#I think that works
-		#mv temp3.covmodel ./DMRtest/"$name".covdf
 
                 wait
 		
